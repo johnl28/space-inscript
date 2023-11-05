71d64ba5d3e4326b600d7943e96c2ace601b2963
John Criste <54412955+johnl28@users.noreply.github.com>
2023-11-05 21:44:38 +0000

    Add data casting

--- SpaceInscript/src/Renderer.h
+++ SpaceInscript/src/Renderer.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <iostream>
+
 #include "Core.h"
+#include "Entity.h"
+
 
 inline void PrintToCoordinates(int x, int y, const char* format, ...)
 {
 	HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
-
 	DWORD dwWritten = 0;
-	COORD cursor = { x, y };
-	WriteConsoleOutputCharacter(hout, format, strlen(format), cursor, &dwWritten);
+	COORD cursor = { static_cast<short>(x), static_cast<short>(y) };
+	WriteConsoleOutputCharacterA(hout, static_cast<LPCSTR>(format), strlen(format), cursor, &dwWritten);
 }
 
 class Renderer
@@ -23,6 +26,8 @@ private:
 private:
 	int m_width;
 	int m_height;
+
+	std::vector<char> m_buffer;
 };
 
 
