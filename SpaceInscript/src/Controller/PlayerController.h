a4dbcd0eb21619d9253203c1ac6f335c05049798
Johnl <ionutcriste28@gmail.com>
2023-11-25 19:04:23 +0000

    Prevent Player from moving out of the GameView

--- SpaceInscript/src/Controller/PlayerController.h
+++ SpaceInscript/src/Controller/PlayerController.h
@@ -1,25 +1,44 @@
 #pragma once
 #include "Controller.h"
 #include "Core.h"
-#include "Actor.h"
+#include "GameView.h"
+#include "Entity/Actor.h"
 
 class PlayerController: public Controller
 {
 public:
 	PlayerController(std::shared_ptr<Actor> actor) : Controller(actor) {}
 
-
 	void Update() override
 	{
-		if (GetAsyncKeyState('W') & 0x8000)
+ 
+		int x = GameView::GetInstance()->GetWidth() - m_actor->GetX();
+		int y = GameView::GetInstance()->GetHeight() - m_actor->GetY();
+
+
+		if (GetAsyncKeyState('W') & 0x8000 && y != GameView::GetInstance()->GetHeight())
 		{
 			m_actor->MoveUp();
 		}
-		else if (GetAsyncKeyState('S') & 0x8000)
+		if (GetAsyncKeyState('S') & 0x8000 && y > 1)
 		{
 			m_actor->MoveDown();
 		}
+		if (GetAsyncKeyState('A') & 0x8000 && x != GameView::GetInstance()->GetWidth())
+		{
+			m_actor->MoveLeft();
+		}
+		if (GetAsyncKeyState('D') & 0x8000 && x > 1)
+		{
+			m_actor->MoveRight();
+		}
 
+#if _DEBUG
+		if (GetAsyncKeyState('R') & 0x8000)
+		{
+			m_actor->SetXY(0, 0);
+		}
+#endif
 	}
 };
 
